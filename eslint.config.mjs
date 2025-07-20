85ef47d16ab0231dd904a245fd3924c7624e6c99
André Marinho <andre_lmarinho@hotmail.com>
2025-07-20 11:46:34 -0300

    chore: integrate Prettier via eslint-plugin-prettier and enable prettier rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
-// eslint.config.js
+// eslint.config.mjs
 import eslintPluginReact from 'eslint-plugin-react';
 import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
 import eslintPluginTypeScript from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default [
   {
@@ -16,7 +17,7 @@ export default [
     ],
   },
   {
-    files: ['**/*.ts', '**/*.tsx'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
@@ -29,8 +30,11 @@ export default [
       react: eslintPluginReact,
       'react-hooks': eslintPluginReactHooks,
       '@typescript-eslint': eslintPluginTypeScript,
+      prettier: eslintPluginPrettier,
     },
+    extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
     rules: {
+      'prettier/prettier': 'error',
       'react/react-in-jsx-scope': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-unused-vars': 'warn',
