2602a293c49cfa41aeed6e0112e00eeb2883c306
André Marinho <107189254+andre-lmarinho@users.noreply.github.com>
2025-08-24 12:25:29 -0300

    ⏪ Revert API routing configuration

---
+++ src/app/api/autocomplete/route.ts
@@ -0,0 +1,2 @@
+// src/app/api/autocomplete/route.ts
+export { GET } from '@/server/api/autocomplete/route';
---
+++ src/app/api/search/route.ts
@@ -0,0 +1,2 @@
+// src/app/api/search/route.ts
+export { GET } from '@/server/api/search/route';
