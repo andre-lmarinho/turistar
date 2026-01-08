c4d9864a4d828c97f06c44b25368436327a1eab6
andre-lmarinho <andre_lmarinho@hotmail.com>
2026-01-08 11:29:29 -0300

    refactor(auth): update auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,13 @@
-import type { ReactNode } from 'react';
+import type { ReactNode } from "react";
 
-import { ClientProviders } from '@/app/providers';
+import { ClientProviders } from "@/app/providers";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <ClientProviders>
-      <main className="min-h-screen">{children}</main>
+      <main className="py-12 bg-card flex min-h-screen flex-col items-stretch justify-center">
+        {children}
+      </main>
     </ClientProviders>
   );
 }
