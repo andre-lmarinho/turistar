e0adbf0264ea4530469b53cae4fbfea00b97c778
andre-lmarinho <andre_lmarinho@hotmail.com>
2026-06-29 17:01:04 -0300

    chore: fix lint and typecheck

--- vitest.setup.tsx
+++ vitest.setup.tsx
@@ -2,6 +2,7 @@ process.env.TZ = "UTC";
 process.env.NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "http://localhost:54321";
 process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "anon";
 process.env.NEXT_PUBLIC_GEOAPIFY_KEY = process.env.NEXT_PUBLIC_GEOAPIFY_KEY ?? "test-key";
+
 import "@testing-library/jest-dom";
 import type React from "react";
 import { createElement } from "react";
